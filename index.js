4704d89f086d347f2c73e16127a4baecf3a0ac2a
Larry Motuzis <larrymotuzis@gmail.com>
2022-01-15 10:57:54 -0600

    commit tree d496ee986440fffa9f92f667944325e5321fd5f0

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
+function scuberGreetingForFeet(feet){
+    if (feet <= 400)  {
+      return "This one is on me!"
+    } else if (feet > 2500) {
+      return "No can do."
+    } else if (feet > 2000) {
+      return "I will gladly take your thirty bucks."
+    }
+  }
+    
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function ternaryCheckCity(city) {
+   return city === 'NYC' ? 'Ok, sounds good.' : 'No go.'
+}
+// function switchOnCharmFromTip(myTip){
+//     if (myTip === 'generous'){
+//       return 'Thank you so much.';
+//       } else if (myTip === 'not as generous'){
+//         return 'Thank you.'
+//       } else {
+//         return 'Bye.'
+//       }
+
+function switchOnCharmFromTip(myTip) {
+    switch(myTip) {
+      case "generous":
+        return "Thank you so much.";
+      case "not as generous":
+        return "Thank you.";
+      default: 
+        return "Bye."
+    }
 }
